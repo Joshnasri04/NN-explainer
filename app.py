639b47ddd53b7d26d730c92045540090d86834ea
JoshnaSri <joshnasripolisetti@gmail.com>
2025-12-31 10:47:00 +0530

    Initialize app.py with imports and TensorFlow setup

---
+++ app.py
@@ -0,0 +1,9 @@
+# Imports (tools we need)
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Make results repeatable (so you see similar numbers each time)
+tf.keras.utils.set_random_seed(42)
+
+print("TensorFlow version:", tf.__version__)
